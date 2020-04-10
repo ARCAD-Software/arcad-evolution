24728bfc0e63425f33cd86ee91042676a4d1eff2
Sébastien JULLIAND <sjulliand@arcadsoftware.com>
2020-04-10 12:12:49 +0200

    Introducing bug for testing purpose

--- bundles/aev.core/src/com/arcadsoftware/aev/core/EvolutionCorePlugin.java
+++ bundles/aev.core/src/com/arcadsoftware/aev/core/EvolutionCorePlugin.java
@@ -14,7 +14,7 @@ import com.arcadsoftware.aev.core.tools.ArcadCoreUtilsHelper;
 import com.arcadsoftware.aev.core.tools.CoreLabels;
 import com.arcadsoftware.aev.core.tools.IHelperLabel;
 import com.arcadsoftware.aev.core.tools.Utils;
-
+sqdsd
 /**
  * The main plugin class to be used in the desktop.
  */
