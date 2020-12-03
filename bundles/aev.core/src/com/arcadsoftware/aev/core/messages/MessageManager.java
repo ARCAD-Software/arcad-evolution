fdd65eec0fee25a0d94d5ea23ffa8160ef6d8925
Seb Julliand <sjulliand@arcadsoftware.com>
2020-12-03 23:46:45 +0100

    Removed unused import

--- bundles/aev.core/src/com/arcadsoftware/aev/core/messages/MessageManager.java
+++ bundles/aev.core/src/com/arcadsoftware/aev/core/messages/MessageManager.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
