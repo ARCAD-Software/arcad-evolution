1213920a996e7357566ee6035f92446df8985cfd
Seb Julliand <sjulliand@arcadsoftware.com>
2021-02-10 09:24:18 +0100

    Allow multiple directories for classloading + export package

--- bundles/aev.core/src/com/arcadsoftware/aev/core/classloader/DirectoriesClassLoader.java
+++ bundles/aev.core/src/com/arcadsoftware/aev/core/classloader/DirectoriesClassLoader.java
@@ -8,26 +8,32 @@ import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.arcadsoftware.aev.core.messages.MessageManager;
 
-public class DirectoryClassLoader extends URLClassLoader {		
-	private static URL[] getURLs(final String path){
-		try (final Stream<Path> stream = Files.walk(Paths.get(path), Integer.MAX_VALUE)){
-			return stream.map(Path::toFile) //
-						.filter(DirectoryClassLoader::isJarOrClass) //
-						.map(DirectoryClassLoader::toURL) //
+public class DirectoriesClassLoader extends URLClassLoader {
+	private static URL[] getURLs(final String...paths){
+		final Set<URL> urls = new HashSet<>();
+		Stream.of(paths).map(Paths::get).forEach(path -> loadURLs(urls, path));
+		return urls.toArray(new URL[urls.size()]);
+	}
+	
+	private static void loadURLs(final Set<URL> urls, final Path path) {
+		try (final Stream<Path> stream = Files.walk(path, Integer.MAX_VALUE)){
+			stream.map(Path::toFile) //
+						.filter(DirectoriesClassLoader::isJarOrClass) //
+						.map(DirectoriesClassLoader::toURL) //
 						.filter(Objects::nonNull) //
 						.collect(Collectors.toSet())
-						.toArray(new URL[0]);
+						.forEach(urls::add);
 		} catch (IOException e) {
 			MessageManager.addAndPrintException(e);
-		}		
-		
-		return new URL[0];
+		}
 	}
 	
 	private static boolean isJarOrClass(final File file){
@@ -43,7 +49,7 @@ public class DirectoryClassLoader extends URLClassLoader {
 		}
 	}
 	
-	public DirectoryClassLoader(final String path) {
-		super(getURLs(path), ClassLoader.getSystemClassLoader());
+	public DirectoriesClassLoader(final String...paths) {
+		super(getURLs(paths), ClassLoader.getSystemClassLoader());
 	}
 }
