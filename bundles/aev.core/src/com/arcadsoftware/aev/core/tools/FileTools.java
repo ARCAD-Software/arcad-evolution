12c86ce023becb2894c37870ea35d07afb226b1a
Seb Julliand <sjulliand@arcadsoftware.com>
2021-02-15 08:40:48 +0100

    Added FileTools::deleteDirectory

--- bundles/aev.core/src/com/arcadsoftware/aev/core/tools/FileTools.java
+++ bundles/aev.core/src/com/arcadsoftware/aev/core/tools/FileTools.java
@@ -2,20 +2,26 @@ package com.arcadsoftware.aev.core.tools;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import com.arcadsoftware.aev.core.messages.MessageManager;
 
 /**
- * Collection of file tools to centralize handling/logging of File related methods.
+ * Collection of file tools to centralize handling/logging of File related
+ * methods.
  * 
  * @author ACL
  *
  */
 public class FileTools {
 
-	private FileTools() {}
-	
+	private FileTools() {
+	}
+
 	public static boolean deleteFile(File file) {
 		try {
 			Files.delete(file.toPath());
@@ -25,27 +31,51 @@ public class FileTools {
 		}
 		return false;
 	}
-	
+
 	public static boolean createNewFile(File file) throws IOException {
 		boolean created = file.createNewFile();
-		// This is only false if the file is not created, otherwise it should throw an error...
+		// This is only false if the file is not created, otherwise it should
+		// throw an error...
 		if (!created)
 			MessageManager.logDiagnostic("File " + file.getName() + " already exists");
 		return created;
 	}
-	
+
 	public static boolean renameTo(File file, File dest) {
 		boolean renamed = file.renameTo(dest);
 		if (!renamed)
 			MessageManager.logDiagnostic("Could not rename file " + file.getName() + " to " + dest.getName());
 		return renamed;
 	}
-	
+
 	public static boolean setReadOnly(File file) {
 		boolean readOnly = file.setReadOnly();
 		if (!readOnly)
 			MessageManager.logDiagnostic("Could not set " + file.getName() + " to Read Only");
 		return readOnly;
 	}
-	
+
+	public static boolean deleteDirectory(final File file) throws IOException {
+		try {
+			Files.walkFileTree(file.toPath(), new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult postVisitDirectory(final Path dir, final IOException exc) throws IOException {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+					Files.delete(file);
+					return FileVisitResult.CONTINUE;
+				}
+			});
+
+			return true;
+		} catch (IOException e) {
+			MessageManager.addAndPrintException(e);
+			return false;
+		}
+	}
+
 }
