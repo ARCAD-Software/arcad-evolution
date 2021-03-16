aa51701ed8927ef029d234fdc128bf138dbde310
Seb Julliand <sjulliand@arcadsoftware.com>
2021-03-16 11:51:06 +0100

    Made MessageDialogService into an interface

--- bundles/aev.core.ui/src/com/arcadsoftware/aev/core/ui/services/MessageDialogService.java
+++ bundles/aev.core.ui/src/com/arcadsoftware/aev/core/ui/services/MessageDialogService.java
@@ -6,52 +6,52 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
-public abstract class MessageDialogService {
+public interface MessageDialogService {
+	
+	public String getApplicationTitle();
 
-	protected abstract String getApplicationTitle();
-
-	private Shell getPluginShell() {
+	default Shell getPluginShell() {
 		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
 	}
 
-	public boolean openConfirm(final String message) {
+	default boolean openConfirm(final String message) {
 		return MessageDialog.openConfirm(getPluginShell(), getApplicationTitle(), message);
 	}
 
-	public boolean openConfirm(final String title, final String message) {
+	default boolean openConfirm(final String title, final String message) {
 		return MessageDialog.openConfirm(getPluginShell(), title, message);
 	}
 
-	public void openError(final String message) {
+	default void openError(final String message) {
 		openError(message, (Throwable) null);
 	}
 
-	public void openError(final String title, final String message) {
+	default void openError(final String title, final String message) {
 		MessageDialog.openError(getPluginShell(), title, message);
 	}
 
-	public void openError(final String message, final Throwable throwable) {
+	default void openError(final String message, final Throwable throwable) {
 		MessageDialog.openError(getPluginShell(), getApplicationTitle(), Optional.ofNullable(message).orElse("")
 				+ Optional.ofNullable(throwable).map(t -> "\n\n" + t.getMessage()).orElse(""));
 	}
 
-	public void openInformation(final String message) {
+	default void openInformation(final String message) {
 		MessageDialog.openInformation(getPluginShell(), getApplicationTitle(), message);
 	}
 
-	public void openInformation(final String title, final String message) {
+	default void openInformation(final String title, final String message) {
 		MessageDialog.openInformation(getPluginShell(), title, message);
 	}
 
-	public boolean openQuestion(final String message) {
+	default boolean openQuestion(final String message) {
 		return MessageDialog.openQuestion(getPluginShell(), getApplicationTitle(), message);
 	}
 
-	public void openWarning(final String message) {
+	default void openWarning(final String message) {
 		MessageDialog.openWarning(getPluginShell(), getApplicationTitle(), message);
 	}
 
-	public void openWarning(final String title, final String message) {
+	default void openWarning(final String title, final String message) {
 		MessageDialog.openWarning(getPluginShell(), title, message);
 	}
 }
