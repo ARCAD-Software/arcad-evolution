7f6d41b782f412a90b1db15100740b9eadb666c7
Seb Julliand <sjulliand@arcadsoftware.com>
2021-10-11 17:45:56 +0200

    Updated ant task after last commit

--- ant/aev.ant.tasks/src/main/java/com/arcadsoftware/mmk/anttasks/taskdefs/misc/AntMessageFormatter.java
+++ ant/aev.ant.tasks/src/main/java/com/arcadsoftware/mmk/anttasks/taskdefs/misc/AntMessageFormatter.java
@@ -3,7 +3,7 @@ package com.arcadsoftware.mmk.anttasks.taskdefs.misc;
 import com.arcadsoftware.ae.core.logger.formatter.AbstractMessageFormatter;
 import com.arcadsoftware.ae.core.logger.messages.Messages;
 
-public class AntMessageFormatter extends AbstractMessageFormatter {
+public class AntMessageFormatter implements AbstractMessageFormatter {
 
 	@Override
 	public String format(final Messages messages) {
