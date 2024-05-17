1e5f54f8699fda9818b2810402f6f39de2215dcb
Seb Julliand <sjulliand@arcadsoftware.com>
2024-05-17 10:51:07 +0200

    Re-introduced AEV RAP feature

---
+++ bundles/aev.core.ui.rap/src/com/arcadsoftware/aev/core/ui/common/FormToolKitUtils.java
@@ -0,0 +1,17 @@
+package com.arcadsoftware.aev.core.ui.common;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+public class FormToolKitUtils {
+
+	public static void paintBordersFor(final FormToolkit toolkit, final Composite composite) {
+		// toolkit.paintBordersFor(composite);
+		// there is nothing to do when using RAP.
+	}
+
+	public static void setOrientationRightToLeft(final FormToolkit toolkit) {
+		// Not supported by RAP !
+		// toolkit.setOrientation(SWT.RIGHT_TO_LEFT);
+	}
+}
